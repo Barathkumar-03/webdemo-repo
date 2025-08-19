ea31d363fecea6e508249218d1e05bad0753c3b0
Barath Kumar Kumar <168456667+Barathkumar-03@users.noreply.github.com>
2025-08-18 18:17:20 -0700

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,11 +1,2 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# Keep packages current
-sudo dnf -y update
-
-# Install & prepare nginx only
-sudo dnf -y install nginx
-sudo systemctl enable nginx
-# stop before CodeDeploy copies files into /usr/share/nginx/html
-sudo systemctl stop nginx || true
+#!/bin/bash
+sudo yum update -y
