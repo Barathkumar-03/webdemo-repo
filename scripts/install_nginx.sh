d5282007841937e264f6671515294b4f9086a946
Barath Kumar Kumar <168456667+Barathkumar-03@users.noreply.github.com>
2025-08-18 17:24:12 -0700

    Update install_nginx.sh

--- scripts/install_nginx.sh
+++ scripts/install_nginx.sh
@@ -1,5 +1,6 @@
-set -e
+#!/usr/bin/env bash
+set -euo pipefail
 sudo dnf update -y
-sudo dnf install -y nginx
+sudo dnf install -y nginx curl
 sudo systemctl enable nginx
 sudo systemctl start nginx
